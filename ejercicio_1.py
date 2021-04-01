374060f5faea5be0f4a6ec15ed160245dbebe5c1
yurley sanchez <yurley.sanchez@yurleys-MacBook-Pro.local>
2021-03-31 19:58:35 -0500

    add taller

---
+++ ejercicio_1.py
@@ -0,0 +1,23 @@
+'''
+1) Implementar la logica de una institucion educativa.
+
+Profesores
+Estudiantes
+Materias
+Notas
+Horarios de Clase (*opcional)
+estas entidades deben ser implementadas mediante objetos. el programa debe permitir las siguientes funciones:
+
+1) un profesor debe poder ver las materias que tienen asignadas (un profesor puede dictar varias clases pero debe al menos dar una)
+
+2) Una materia puede ser dictada por varios profesores y pueden estar inscritos varios estudiantes
+
+3) un estudiante puede estar inscritos en muchas materias
+
+4) las notas son ingresadas por el profesor que dicta la materia en la clase donde el estudiante este incrito
+
+5) los estudiantes deben poder ver que notas tienen y ver un promedio por materia y general en todas sus materias
+
+6) {OPCIONAL} - los horarios son definidos de maximo 6 horas diarias, un estudiante no puede estar en dos materias con el mismo horario, un profesor no puede dictar dos materias que sean vistas el mismo
+
+'''
