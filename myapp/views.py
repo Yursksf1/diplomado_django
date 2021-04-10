675cab71e38303084695d2c5bc29b0c4c7faeae0
yurley sanchez <yurley.sanchez@yurleys-MacBook-Pro.local>
2021-04-10 11:00:33 -0500

    usign models in views

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Subject
 
 # Create your views here.
 
-def index(request):
+
+def subject(request):
+    subjects = Subject.objects.all()
+    response = ''
+    for subject in subjects:
+        print(subject.name)
+        response = response + ' ' + subject.name
+
     print('estoy llamando al index')
-    return HttpResponse('hello')
+    return HttpResponse(response)
