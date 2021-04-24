b3d9ca1763f9f6cf55e69394c722ee9347962d96
yurley sanchez <yurley.sanchez@yurleys-MacBook-Pro.local>
2021-04-24 11:31:38 -0500

    add validation login

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login',   views.login,    name='login'),
-    path('saludo', views.saludo,    name='saludo'),
+    path('login/',    views.login,     name='login'),
+    path('logout/',   views.logout,    name='logout'),
+    path('saludo/',   views.saludo,    name='saludo'),
+    path('info/',     views.info,      name='info'),
 ]
--- user/views.py
+++ user/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
@@ -12,11 +13,18 @@ def login(request):
         user = authenticate(username=username, password=password)
         if user:
             auth_login(request, user)
-
             return redirect('saludo')
 
     return render(request, 'login.html')
 
 
+def logout(request):
+    auth_logout(request)
+    return redirect('saludo')
+
 def saludo(request):
     return render(request, 'saludo.html')
+
+@login_required(login_url='/user/login/')
+def info(request):
+    return render(request, 'info.html')
